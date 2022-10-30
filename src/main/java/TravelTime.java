be97f688c26650581cf282a920d3375635a2ab3d
Ilya Danilov <esozap32@mail.ru>
2022-10-30 22:19:39 +0400

    TravelTime

--- src/main/java/TravelTime.java
+++ src/main/java/TravelTime.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class TravelTime {
+public class TravelTime {
+    public static void main(String[] args) {
+        int distance = 55;
+        double averageSpeed = 50.6;
+        double time = distance / averageSpeed;
+        double theCoefficientOfDelay = 0.88;
+        double eveningTime = time / theCoefficientOfDelay;
+        System.out.println("Тестировщик добирается до дома вечером за " + eveningTime + " часа");
+    }
 }
